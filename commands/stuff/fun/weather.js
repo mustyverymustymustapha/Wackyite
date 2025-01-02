218dc23e8f4616019269e17da2cdd60ba73e5643
mustyverymustymustapha <mustaphasatti1@gmail.com>
2025-01-02 20:34:11 +0500

    add weather command to fun category [NOT WORKING]

---
+++ commands/stuff/fun/weather.js
@@ -0,0 +1,7 @@
+/* const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('')
+} */ // this is an attempt of making a weather command, however it's incomplete and disfunctional
