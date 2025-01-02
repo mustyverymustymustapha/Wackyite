85d098a3dcb1db2f01b8273d400c67f0d4addc40
mustyverymustymustapha <mustaphasatti1@gmail.com>
2025-01-02 21:04:33 +0500

    remove incomplete guessthenumber and weather commands due to incompatibility with latest discord.js version

--- commands/stuff/fun/weather.js
+++
@@ -1,7 +0,0 @@
-/* const { SlashCommandBuilder } = require('discord.js');
-const axios = require('axios');
-
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('')
-} */ // this is an attempt of making a weather command, however it's incomplete and disfunctional
